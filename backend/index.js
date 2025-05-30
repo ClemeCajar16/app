a2ad13c0656c1977ca3c56dc3c197852b37f662e
Cleme Cajar <ccajarlaguna@gmail.com>
2025-05-30 12:37:21 -0500

    se conecto el purto

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
 import express from "express";
+import  cors from "cors";
+import  Mongoose  from "mongoose";
+import  morgan  from "morgan";
+
+const app = express();
+
+app.get("/", (req, res) => {
+
+    res.send("Hello World!");
+});
+
+const POST = process.env.POST || 3000;
+
+app.listen(3000, () => {
+    console.log(`server is runing on port ${POST} 😎`);
+});
