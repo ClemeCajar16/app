12bb45292e75207e240e50908dacef0edbb23437
Cleme Cajar <ccajarlaguna@gmail.com>
2025-05-30 19:14:44 -0500

    se conecto el purto

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import  cors from "cors";
-import  Mongoose  from "mongoose";
 import  morgan  from "morgan";
+import { mongoose } from "./src/DB.js";
 
 const app = express();
+
+app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors({"http://localhost:4000": true}));
+
 
 app.get("/", (req, res) => {
 
     res.send("Hello World!");
 });
+
+
 
 const POST = process.env.POST || 3000;
 
