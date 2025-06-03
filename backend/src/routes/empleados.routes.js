4c706441d57391f24afd37bcecf40c46c6c85fde
Cleme Cajar <ccajarlaguna@gmail.com>
2025-06-03 17:02:41 -0500

    se esta creando los esquemas

--- backend/src/routes/empleados.routes.js
+++ backend/src/routes/empleados.routes.js
@@ -1,1 +1,33 @@
 import mongoose from "mongoose";
+
+const {schema} = mongoose;
+
+export const empleadoSchema = new schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    oficio: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    telefono: {
+        type: String,
+        required: true
+    },
+
+    direccion: {
+        type: String,
+        required: true
+    },
+    salario: {
+        type: Number,
+        required: true
+    }
+})
+
