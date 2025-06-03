3de4fbd2bb431ff9561fc1d61e58675099cb2ecb
Cleme Cajar <ccajarlaguna@gmail.com>
2025-06-03 10:54:14 -0500

    creacion de los metodos

--- backend/src/controllers/empleado.controllers.js
+++ backend/src/controllers/empleado.controllers.js
@@ -1,1 +1,19 @@
-import { empleado } from "../../models/empleado.models.js";
+import { empleado } from "../../schemas/empleado.schemas.js";
+
+const empleadosController = {};
+
+empleadosController.getEmpleados = async (req, res) => {
+  try {
+    const empleados = await empleado.find();
+    res.status(200).json(empleados);
+  } catch (error) {
+    console.error("Error fetching empleados:", error);
+    res.status(500).json({ message: "Error fetching empleados" });
+  }
+}
+
+empleadosController.createEmpleado = async (req, res) => {
+    const empleados = new empleado(req.body);
+    await empleados.save()
+    res.sratsus(201).send("se ha creado un empleado");
+}
